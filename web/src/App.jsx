1de1b4d958e61877bb9dc4ff4d5810834a7de11b
bradley <16675719+B44ken@users.noreply.github.com>
2026-04-21 07:55:16 -0400

    use mp4

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -2,25 +2,19 @@ import { App, Btn, Card, D, H1, Input } from 'b44ui'
 import { useState } from 'react'
 
 export default () => {
-  const [ix, setIx] = useState(0)
-  const [em, setEm] = useState('')
-
-  const reg = () => {
-    fetch('https://boratto.ca/botblocks/signup?email=' + em)
-      .then(x => setIx(2))
-  }
+  const [ix, setIx] = useState(0), [em, setEm] = useState('')
+  const sub = () => fetch('https://boratto.ca/hit/bksignup?em='+em).then(x => setIx(2))
 
   return <App align='center'>
     <H1>botblocks</H1>
     is a platform that makes it incredibly easy to build robots.
 
-    <Card wd={0.6}> <video controls src='bk.mov' /> </Card>
+    <Card wd={0.6}> <video controls src='bk.mp4' /> </Card>
 
     {ix == 0 && <Btn click={() => setIx(1)}>learrn more</Btn>}
     {ix == 1 && <D row gap={4}>
-      <Input state={[em, setEm]} placeholder='email...' /> <Btn click={reg}>subscribe</Btn>
+      <Input state={[em, setEm]} placeholder='email..' /> <Btn click={sub}>subscribe</Btn>
     </D> }
     {ix == 2 && <D>{`you'll get emails for new announcements!`}</D>}
-
   </App>
 }
